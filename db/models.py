5ada8dc8198a8e0808402d7a6fc59990b894d7ce
Thana B <thana.b@outlook.jp>
2022-05-26 02:00:34 +0800

    add environment variable

--- db/models.py
+++ db/models.py
@@ -1,11 +1,24 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String
-
+from sqlalchemy import Column, Integer, String, DateTime # data type
+from sqlalchemy import ForeignKey # tool
+from sqlalchemy.orm import relationship
 
 class DbUser(Base):
     __tablename__ = "user"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String)
-    email = Column(String)
-    password = Column(String)
+    id          = Column(Integer, primary_key = True, index = True)
+    username    = Column(String)
+    email       = Column(String)
+    password    = Column(String)
+    items       = relationship('DbPost', back_populates = 'user')
+
+class DbPost(Base):
+    __tablename__ = "post"
+
+    id                = Column(Integer, primary_key = True, index = True)
+    image_url         = Column(String)
+    image_url_type    = Column(String)
+    caption           = Column(String)
+    timestamp         = Column(DateTime)
+    user_id           = Column(Integer, ForeignKey('user.id'))
+    user              = relationship('DbUser', back_populates = 'items')
--- routers/schemas.py
+++ routers/schemas.py
@@ -1,15 +1,43 @@
 from pydantic import BaseModel
-
+from datetime import datetime
 
 class UserBase(BaseModel):
-    username: str
-    email: str
-    password: str
+    class Config():
+        orm_mode = True
+    username    : str
+    email       : str
+    password    : str
 
 
 class UserDisplay(BaseModel):
+    class Config():
+        orm_mode = True
+
+    username    : str
+    email       : str
+
+
+class PostBase(BaseModel):
+    class Config():
+        orm_mode = True
+
+    image_url         : str
+    image_url_type    : str
+    caption           : str
+    creator_id        : int
+
+# For PostDisplay
+class User(BaseModel):
     username: str
-    email: str
+
+
+class PostDisplay(BaseModel):
+    image_url         : str
+    image_url_type    : str
+    caption           : str
+    creator_id        : int
+    timestamp: datetime
+    user: User
 
     class Config:
         orm_mode = True

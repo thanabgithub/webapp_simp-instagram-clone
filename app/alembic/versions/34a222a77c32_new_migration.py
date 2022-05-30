34c73e40b48f66a3b3076db643518823250e7e43
Thana B <thana.b@outlook.jp>
2022-05-31 02:26:28 +0800

    include nginx

--- app/alembic/versions/34a222a77c32_new_migration.py
+++ app/alembic/versions/34a222a77c32_new_migration.py
@@ -8,7 +8,6 @@ Create Date: 2022-05-29 17:19:05.628695
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision = '34a222a77c32'
 down_revision = '2990c621de17'

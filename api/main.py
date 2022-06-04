318fb801aba79f9d03d223c2898b29c7f129a32d
Thana B <thana.b@outlook.jp>
2022-06-05 04:10:01 +0800

    frontend

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi_sqlalchemy import DBSessionMiddleware, db
+from starlette.middleware.cors import CORSMiddleware
 
 from db import models
 from db.database import engine
@@ -7,6 +8,43 @@ from routers import user, post, comment
 import os
 from fastapi.staticfiles import StaticFiles
 from auth import authentication
+from starlette.middleware import Middleware
+
+from starlette.middleware import Middleware
+
+
+
+origins = [
+    "http://front.tiangolo.com",
+    "https://front.tiangolo.com",
+    "http://front",
+    "http://front:3000",
+    "https://front",
+    "https://front:3000",
+
+
+    "front",
+    "front:8000",
+
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+    "https://localhost",
+    "https://localhost:3000",
+
+    "http://localhost",
+    "http://localhost:80",
+    "https://localhost",
+    "https://localhost:443",
+    "http://8.219.139.91",
+
+
+]
+middleware = [
+    Middleware(CORSMiddleware, allow_origins=origins)
+]
+
 
 app = FastAPI()
 
@@ -24,6 +62,15 @@ def root():
     return "Hello world"
 
 
+
 models.Base.metadata.create_all(engine)
 
 app.mount("/images", StaticFiles(directory="images"), name="images")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+const BASE_URL = "http://api:8000/";
+// const BASE_URL = "http://api-insta.thana.team:80/";
+
 
 function App() {
-  return ("Hello world"
-  );
+  const [post, setPosts] = useState([]);
+  console.log(BASE_URL + "post/all/");
+  useEffect(() => {
+    fetch(BASE_URL + "post/all")
+      .then((response) => {
+        console.log(response.json());
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+       setPosts(data);
+      })
+      .catch((error) => {
+     console.log(error);
+      alert(error);
+      });
+  }, []);
+  return "Hello world";
 }
 
 export default App;
